96192da556e0f8db427cd4a079c5b3bc9547bf84
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-07-07 14:51:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ import streamlit.components.v1 as components
 
 # flowise chatbot
 components.html(
-  <flowise-fullchatbot></flowise-fullchatbot>
-<script type="module">
+ <script type="module">
     import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
-    Chatbot.initFull({
+    Chatbot.init({
         chatflowid: "2da51f59-dc6b-45f7-a032-66598b1af915",
         apiHost: "https://kognitio.onrender.com",
     })
